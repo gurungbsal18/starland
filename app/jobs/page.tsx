00e8cafb5e0329ca2d8974b589f75d0517498b75
gurungbsal18 <gurungbsal18@gmail.com>
2025-02-27 00:35:12 +0545

    search added on all job page

--- app/jobs/page.tsx
+++ app/jobs/page.tsx
@@ -3,22 +3,43 @@ import React, { useState } from "react";
 import JobsData from "@/components/data/JobsData";
 import { Button } from "@heroui/button";
 import JobsModal from "@/components/modal/JobsModal";
+import { Input } from "@heroui/input";
 
 export default function JobsPage() {
   const [isOpen, setIsOpen] = useState(false);
   const [selectedJob, setSelectedJob] = useState(null);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchInput, setSearchInput] = useState("");
 
   const handleModal = (job: any) => {
     setSelectedJob(job);
     setIsOpen(true);
   };
 
+  const handleSearch = () => {
+    setSearchQuery(searchInput);
+  };
+
+  const filterJobs = JobsData.filter((job) =>
+    job.position.toLowerCase().includes(searchQuery.toLowerCase()),
+  );
+
   return (
     <div>
-      <h1 className="text-4xl font-bold my-8">Jobs</h1>
-      {JobsData.length > 0 ? (
-        <div className="grid grid-cols-12 gap-4">
-          {JobsData.map((list) => (
+      <div>
+        <h1 className="text-4xl font-bold my-8">Jobs</h1>
+        <div className="flex gap-2 items-center">
+          <Input
+            placeholder="search"
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
+          />
+          <Button onPress={handleSearch}>Search</Button>
+        </div>
+      </div>
+      {filterJobs.length > 0 ? (
+        <div className="grid grid-cols-12 gap-4 mt-5">
+          {filterJobs.map((list) => (
             <div
               key={list.id}
               className="col-span-12 md:col-span-3 bg-slate-200 rounded-md p-4 shadow-md"
@@ -27,7 +48,7 @@ export default function JobsPage() {
                 {list.position}
               </h4>
               <p className="text-black">NPR {list.salary}</p>
-              <p className="">Total Hiring: {list.totalHiring}</p>
+              <p className="text-black">Total Hiring: {list.totalHiring}</p>
               <span className="flex justify-between mt-4 gap-4">
                 <Button onPress={() => handleModal(list)} className="flex-grow">
                   View Detail
@@ -40,7 +61,7 @@ export default function JobsPage() {
           ))}
         </div>
       ) : (
-        <>No Dat Found</>
+        <>No jobs Found</>
       )}
 
       <JobsModal
